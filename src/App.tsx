fa9114e09f052d3a41fd47d05190ac7ddff3a61a
Warek01 <alexandrudobrojan@gmail.com>
2022-06-29 12:10:16 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+const App: React.FC = () => {
+	return (
+		<BrowserRouter>
+			123
+		</BrowserRouter>
+	);
+};
+
+export default App;
