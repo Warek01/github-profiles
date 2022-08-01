b384d67777cdf5a586899f1d1482c6df201c6f96
Warek01 <alexandrudobrojan@gmail.com>
2022-08-01 13:10:01 +0300

    Refactored analyzeReposLanguages to use Promise.allSettled

--- src/utils/analyzeReposLanguages.ts
+++ src/utils/analyzeReposLanguages.ts
@@ -1,9 +1,13 @@
 import { GitHubRepo } from '../types'
 
 interface AnalyzedRepoLanguages {
+	/** Languages mapped by characters */
 	raw: Map<string, number>
+	/** Languages mapped by percentage */
 	parsed: Map<string, number>
+	/** Total amount of characters */
 	total: number
+	/** Request response time */
 	elapsedMs: number
 }
 
@@ -19,29 +23,28 @@ const analyzeReposLanguages = async (repos: GitHubRepo[], authToken: string = ''
 	const parsed = new Map<string, number>()
 	let total = 0
 	
-	try {
-		const promises: Promise<Response>[] = []
-		repos.forEach(repo => promises.push(fetch(repo.languages_url, { headers })))
-		
-		const responses = await Promise.all(promises)
-		const values: { [name: string]: number }[] = await Promise.all(responses.map(res => res.json()))
-		
-		values.forEach(obj => {
-			Object.entries(obj).forEach(([key, value]) => {
-				total += value
-				if (raw.has(key)) raw.set(key, raw.get(key)! + value)
-				else raw.set(key, value)
-			})
-		})
-		
-		raw.forEach((value, key) => {
-			const v = Number((value / total * 100).toFixed(2))
-			parsed.set(key, v)
-		})
-		
-	} catch (err) {
+	const promises: Promise<Response>[] = []
+	repos.forEach(repo => promises.push(fetch(repo.languages_url, { headers })))
 	
-	}
+	const responses = await Promise.allSettled(promises)
+	const handledResponses = responses.map(res => {
+		if (res.status === 'rejected') console.log('Could not load', res)
+		else return res.value.json()
+	})
+	const values: { [name: string]: number }[] = await Promise.all(handledResponses)
+	
+	values.forEach(obj => {
+		Object.entries(obj).forEach(([key, value]) => {
+			total += value
+			if (raw.has(key)) raw.set(key, raw.get(key)! + value)
+			else raw.set(key, value)
+		})
+	})
+	
+	raw.forEach((value, key) => {
+		const v = Number((value / total * 100).toFixed(2))
+		parsed.set(key, v)
+	})
 	
 	const elapsedMs = Date.now() - begin
 	
