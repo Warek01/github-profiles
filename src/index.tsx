973a5774fb4fef3c9e1c247ef9e14aa984ad63e5
Warek01 <alexandrudobrojan@gmail.com>
2022-11-13 09:55:46 +0200

    change

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'dotenv/config'
 
 import './index.css'
-
 import App from './App'
+
+const baseurl = process.env['BASE_NAME']
 
 const root = ReactDOM.createRoot(document.querySelector<HTMLElement>('#root')!)
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={baseurl}>
     <App />
   </BrowserRouter>
 )
